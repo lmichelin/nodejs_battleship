f643fa21843df7697f912ca25f8bc51ccbd69c44
Francois Hendriks <francois.hendriks@ensta.fr>
2017-05-02 15:50:52 +0200

    added battleship javascript

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ var express = require('express');
 var port = 8080;
 
 var app = express();
+
+
+app.get('/battleship', function(req, res) {
+  var row = req.param('row');
+  var column = req.param('column');
+
+  res.send('Ligne: ' + row + ' ' + 'Colonne: ' + column);
+});
  
 app.get('/', function (req, res) {
   res.send('Hello World');
