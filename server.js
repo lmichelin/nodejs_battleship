07ce14316f0ac1014145f5233cca64ce6e9ed6cc
Francois Hendriks <francois.hendriks@ensta.fr>
2017-05-02 16:27:58 +0200

    started battleship functions

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
+/* Require Dependencies */
 var express = require('express');
 var handlebars = require('express-handlebars');
 var path = require('path');
+
+/* Define Port 8080 by default */
 var port = 8080;
 
+/* Initialize express for easy server-client relationship */
 var app = express();
 
+/* set view engine as handlebars */
 app.set('views', path.join(__dirname,'views'));
 app.set('view engine', 'handlebars');
 app.engine('handlebars', handlebars());
 
+/* Game route */
 app.get('/battleship', function(req, res) {
   var row = req.param('row');
   var column = req.param('column');
@@ -16,8 +22,11 @@ app.get('/battleship', function(req, res) {
   res.send('Ligne: ' + row + ' ' + 'Colonne: ' + column);
 });
 
+/* Main route */
 app.get('/', function (req, res) {
   res.render('index');
 });
 
+/* Initialize server */
 app.listen(port);
+ 
