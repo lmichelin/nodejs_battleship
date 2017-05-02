8d3950d6b0c1d189ff3560736e527d1ba83610ff
Louis-Marie MICHELIN <louis-marie.michelin@ensta-paristech.fr>
2017-05-02 16:04:08 +0200

    handlebars & views

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var express = require('express');
+var handlebars = require('express-handlebars');
+var path = require('path');
 var port = 8080;
 
 var app = express();
 
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine', 'handlebars');
+app.engine('handlebars', handlebars());
 
 app.get('/battleship', function(req, res) {
   var row = req.param('row');
@@ -10,9 +15,9 @@ app.get('/battleship', function(req, res) {
 
   res.send('Ligne: ' + row + ' ' + 'Colonne: ' + column);
 });
- 
+
 app.get('/', function (req, res) {
-  res.send('Hello World');
+  res.render('index');
 });
- 
+
 app.listen(port);
